b3e5ecaf0e0974ad72545b17ebbc82b92ddcc4c3
DynaMightt327 <malejacarvajalnepta@gmail.com>
2026-02-14 13:49:17 -0500

    otra vezz

---
+++ src/co/edu/unbosque/view/DeletedWindow.java
@@ -0,0 +1,39 @@
+package co.edu.unbosque.view;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DeletedWindow extends JFrame {
+
+	private JButton bBack;
+
+	public DeletedWindow() {
+		initComponents();
+		setVisible(false);
+	}
+
+	public void initComponents() {
+		// CONFIGURACION DE LA VENTANA
+		this.setTitle("Lucky j3");
+		this.setBounds(250, 30, 880, 650);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setLayout(null);
+		this.getContentPane().setBackground(Color.decode("#FFFFFF"));
+
+		// ==BOTONES==
+		bBack = new JButton("Volver");
+		bBack.setBounds(360, 330, 170, 50);
+		bBack.setFont(new Font("Agency FB", Font.BOLD, 22));
+		bBack.setBackground(Color.decode("#130344"));
+		bBack.setForeground(Color.decode("#FFFFFF"));
+		bBack.setBorderPainted(false);
+		bBack.setFocusPainted(false);
+		add(bBack);
+
+	}
+
+}
